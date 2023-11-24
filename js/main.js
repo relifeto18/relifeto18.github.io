dac8cfa5242b3f07709c4dfdeeedd7f3ae648021
yanxi <lyx010318@gmail.com>
2023-11-24 18:41:23 -0500

    update

--- js/main.js
+++ js/main.js
@@ -39,39 +39,74 @@ exclude: 'yes'
 
   })
 
-  let slideIndex = 1;
-  showSlides(slideIndex);
+  let slideIndex1 = 1;
+  let slideIndex2 = 1;
+
+  showSlides1(slideIndex1);
+  showSlides2(slideIndex2);
 
   // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+  function plusSlides1(n) {
+    showSlides1(slideIndex1 += n);
   }
-
+  
+  function plusSlides2(n) {
+    showSlides2(slideIndex2 += n);
+  }
+  
   // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
+  function currentSlide1(n) {
+    showSlides1(slideIndex1 = n);
+  }
+  
+  function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
   }
 
-  function showSlides(n) {
+  function showSlides1(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("demo");
-    let captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    let slides = document.getElementsByClassName("mySlides1");
+    let dots = document.getElementsByClassName("demo1");
+    if (n > slides.length) {slideIndex1 = 1}
+    if (n < 1) {slideIndex1 = slides.length}
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].classList.remove("active");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-    centerScroll();
+    slides[slideIndex1-1].style.display = "block";
+    dots[slideIndex1-1].classList.add("active");
+
+    centerScroll1();
+  }
+  
+  function showSlides2(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides2");
+    let dots = document.getElementsByClassName("demo2");
+    if (n > slides.length) {slideIndex2 = 1}
+    if (n < 1) {slideIndex2 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("active");
+    }
+    slides[slideIndex2-1].style.display = "block";
+    dots[slideIndex2-1].classList.add("active");
+
+    centerScroll2();
   }
 
-  function centerScroll() {
+  function centerScroll1() {
+    let container = document.querySelector('.scroll-container');
+    let selected = document.querySelector('.active');
+    
+    container.scrollLeft = selected.offsetLeft + (selected.offsetWidth / 2) - (container.offsetWidth / 2);
+  }
+
+  function centerScroll2() {
     let container = document.querySelector('.scroll-container');
     let selected = document.querySelector('.active');
     
